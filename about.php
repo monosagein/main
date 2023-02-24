365c5d302346f97a2f6bc3a6aaa6503dd0a03a07
ghani <skghani9999@gmail.com>
2023-02-24 15:42:51 +0530

    3rd commit

--- about.php
+++ about.php
@@ -2,11 +2,12 @@
 include ('admin/db_connect.php')
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
+                                    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> About | ElegoMotors </title>
      <!--favicon -->
